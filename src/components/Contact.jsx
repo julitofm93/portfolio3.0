82588c9c8c56ff82b09c77b4aa6c6505b1bcea50
julitofm93 <julian.fm@live.com>
2023-03-06 13:26:46 -0300

    other sections

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,14 +1,84 @@
 import React from 'react'
 import styled from 'styled-components'
+import Map from './Map'
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
 `
 
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  gap: 50px;
+`
+
+const Left = styled.div`
+  flex: 1;
+`
+
+const Title = styled.h1`
+  font-weight: 200;
+`
+
+const Form = styled.form`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  gap: 25px;
+`
+
+const Input = styled.input`
+  padding: 20px;
+  color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+`
+
+const TextArea = styled.textarea`
+  padding: 20px;
+  color: #e8e6e6;
+  border: none;
+  border-radius: 5px;
+`
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  border: none;
+  font-weight: bold;
+  cursor: pointer;
+  border-radius: 5px;
+  padding: 20px;
+`
+
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+`
+
 const Contact = () => {
   return (
-    <Section>Contact</Section>
+    <Section>
+      <Container>
+        <Left>
+          <Map/>
+        </Left>
+        <Right>
+        <Form>
+            <Title></Title>
+            <Input placeholder='Name'/>
+            <Input placeholder='Email'/>
+            <TextArea placeholder='Write your message' rows={10}/>
+            <Button>Send</Button>
+          </Form>
+        </Right>
+      </Container>
+    </Section>
   )
 }
 
